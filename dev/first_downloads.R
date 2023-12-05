76c2f7a39c692867ab57204a1c36f7f2e9a69b6b
Sara Hänzi <sarahaenzi@gmail.com>
2023-12-05 22:48:13 +0100

    convert date to datetime with ugly workaround

--- dev/first_downloads.R
+++ dev/first_downloads.R
@@ -1,8 +1,11 @@
 library(dplyr)
 config <- config::get()
 
-df22 <- data.table::fread(config$urls$link_2022) |> 
-  janitor::clean_names()
+res <- download_meteo_data(config$urls)
 
-temp_22 <- df22 |> 
-  filter(parameter == "T")
+# why on earth does this not work
+# lubridate::ymd_hm(res$temperature$datum[[1]], format = "%Y-%m-%dT%H:%M", tz = "Europe/Zurich")
+
+rain <- res$rain
+temp <- res$temperature
+         
